d7b342036e9618b2886b5484a9ad5d5ccca8eab0
Vlad Latyshev <152328059+latysvlad@users.noreply.github.com>
2023-12-06 15:01:50 +0300

    Create Second-Occurrence-2.py

---
+++ Simple-Progs/Second-Occurrence/Second-Occurrence-2.py
@@ -0,0 +1,11 @@
+# put your python code here
+
+s = input()
+
+if s.find("f") == -1:
+    print(-2)
+elif s.find("f") == s.rfind("f"):
+    print(-1)
+else:
+    first_f = s.find("f")
+    print(s[first_f + 1:].find("f") + first_f + 1)
